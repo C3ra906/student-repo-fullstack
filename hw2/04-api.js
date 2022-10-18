1a88693a9269954c9871e27c1c5060b6f299bad3
Cera <ceoh@pdx.edu>
2022-10-18 02:11:46 -0700

    04: Got it working. TODO: check formats of all files.

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -3,5 +3,32 @@
 const url = 'https://restcountries.com/v3.1/all';
 
 // Add your code here
+let app = document.querySelector('#results');
+
+const addCountryToDOM = (country) => {
+  let element = document.createElement('li');
+  let name = '';
+  let population = '';
+  name = country.name.official;
+  population = country.population.toLocaleString('en-US');
+  element.innerHTML = `${name} - ${population}`;
+  app.append(element);
+};
+
+const getData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((country) => {
+        addCountryToDOM(country);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      let element = document.createElement('div');
+      element.textContent = 'An error occurred. Please reload.';
+      app.append(element);
+    });
+};
 
 getData(url);
