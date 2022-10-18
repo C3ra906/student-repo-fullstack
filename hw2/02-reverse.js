520b440701d1c19715537e778ba3c2463b3784b6
Cera <ceoh@pdx.edu>
2022-10-18 00:54:56 -0700

    Got 02 working as I want it! Need to figure out 04 still.

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -2,14 +2,53 @@
 
 // Add your code here
 let main = document.querySelector('main');
-let form = document.getElementsByClassName('form-group');
-let number = document.getElementById('input');
-let reverse = document.getElementById('reverse');
 
-number.addEventListener('input', (event) => {
-  if (number.value.length === 8) {
+let number = document.getElementById('input');
+let button = document.getElementById('reverse');
+let new_element = document.createElement('div');
+let result = document.createElement('p');
+let input_array = [];
+let counter = 0;
+result.innerHTML = '';
+new_element.appendChild(result);
+main.appendChild(new_element);
+
+main.addEventListener('input', array);
+
+function array(event) {
+  if (number.value[counter] !== undefined) {
+    input_array.push(number.value[counter]);
+    console.log(`${number.value[counter]} was added to the array`);
+    console.log(input_array);
+    counter += 1;
   }
-});
-reverse.addEventListener('submit', (event) => {
-  event.preventDefault();
-});
+}
+
+button.addEventListener('click', reverse);
+
+function reverse(event) {
+  if (counter !== 8) {
+    result.innerHTML = 'Error: Please input an 8-digit number';
+    result.style.color = 'red';
+    result.style.margin = '10px';
+    number.addEventListener('click', clear);
+  } else {
+    let index = counter - 1;
+    let reversed_num = '';
+    while (index !== -1) {
+      reversed_num += `${input_array[index]}`;
+      index -= 1;
+    }
+    result.innerHTML = `${input.value} --> ${reversed_num}`;
+    result.style.color = 'green';
+    result.style.margin = '10px';
+    number.addEventListener('click', clear);
+  }
+}
+
+function clear(event) {
+  result.innerHTML = '';
+  counter = 0;
+  input_array = [];
+  number.value = '';
+}
