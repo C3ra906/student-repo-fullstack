15218cf64789fbe74a88fd2ca6a8cc721489a2d1
Cera <ceoh@pdx.edu>
2022-10-18 23:37:23 -0700

    03: Added comments. Added one more check for email input. Checked Wave and whether it works in other browsers - all passing.

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,28 +1,40 @@
 /** Exercise 03 - Form **/
-// Add your code here
+
 let form = document.querySelector('form');
 
 form.addEventListener('submit', handleSubmit);
 
+// Function that outputs form data in the console
 function handleSubmit(event) {
+  // Check whether Name and Email fields are empty
   if (form.name.value.length > 0 && form.email.value.length > 0) {
-    console.log('======== Form Submission ========');
-    console.log('Name: ' + form.name.value);
-    console.log('Email: ' + form.email.value);
-    if (form.feedback.value.length > 0) {
-      console.log('Feedback: ' + form.feedback.value);
+    // Check if the email address includes at least @
+    if (form.email.value.includes('@')) {
+      console.log('======== Form Submission ========');
+      console.log('Name: ' + form.name.value);
+      console.log('Email: ' + form.email.value);
+
+      // Check whether the message field is empty and output console message
+      if (form.feedback.value.length > 0) {
+        console.log('Feedback: ' + form.feedback.value);
+      } else {
+        console.log('No feedback was submitted.');
+      }
+
+      // Check whether the newsletter checkbox is checked and output console message
+      if (form.newsletter.checked === true) {
+        console.log('Newsletter: Yes, I would like to join the newsletter.');
+      } else {
+        console.log('No, thank you.');
+      }
     } else {
-      console.log('No feedback was submitted.');
+      // Email was missing at least @
+      console.warn(
+        'Your email address is missing @. Please check your email address is correct'
+      );
     }
-    if (form.newsletter.checked === true) {
-      console.log('Newsletter: Yes, I would like to join the newsleetter.');
-    } else {
-      console.log('No, thank you.');
-    }
-  }
-  //event.preventDefault(); Should we be clearing the form after?
-  else {
-    //Empty name or email
+  } else {
+    // Empty Name or Email
     console.warn('You must enter your name and email to submit this form');
   }
 }
