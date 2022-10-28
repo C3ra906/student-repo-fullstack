4fdff6ec7cd22fa286e124724737e1d1840f130c
Cera <ceoh@pdx.edu>
2022-10-28 14:51:42 -0700

    03 is finally working properly

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -1,51 +1,113 @@
 const http = require('http');
-const fs = require('fs');
-const { Readable } = require('stream');
 const port = process.env.PORT || 5001;
 
 // http://localhost:5001/form should return a form with input elements for username, email, and submit button
-// form
+// http://localhost:5001/submit should return all the data the user entered
+
+// basic form in HTML
 const form = `<html><head><title>Submission Form</title></head><body>
-  <form method='post' action="/submit">
+  <form method='post' action='/submit'>
   <label for="name">Name: </label> 
-  <input type="text" name="name" id="name" /><br />
+  <input type="text" name="Name" id="name" /><br />
   <label for="email">Email: </label>
-  <input type="text" name="email" id="email" /><br />
-  <label for="comments">Comments: </label><br />
-  <textarea name="comments" id="comments"></textarea><br />
-  <input type="checkbox" value="" id="newsletter"/>
-  <label for="newsletter">Sign up for the newsletter</label><br />
+  <input type="text" name="Email" id="email" /><br />
+  <label for="comments">comments: </label><br />
+  <textarea name="Comments" id="comments"></textarea><br />
+  <input type="checkbox" name="Newsletter" id="newsletter"/>
+  <label for="Newsletter">Sign up for the newsletter</label><br />
   <input type="submit" name="submit" value="Submit"/><br />
   </form></body></html>`;
 
 const server = http.createServer((req, res) => {
-  //let info = '';
+  // main route. Just redirects to form
+  if (req.url === '/') {
+    res.writeHead(302, { Location: '/form' });
+    res.end();
+  }
 
   // new route --> form
-  if (req.url === '/form') {
+  else if (req.url === '/form') {
+    //form submitted
     res.writeHead(200, {
       'Content-Type': 'text/html',
     });
     res.write(form);
-    req.on('data', (chunk) => {
-      const writeable = fs.createWriteStream('form.txt');
-    });
-    req.on('end', () => {
-      //res.write(info);
-      res.end();
-    });
+    res.end();
   }
 
   // new route --> submit
   else if (req.url === '/submit') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
+    let info = '<p>';
+    let original_url = new URL(req.url, `http://${req.headers.host}`);
+    let new_url = '';
+    let news = '';
+    // Handles data
+    req.on('data', (chunk) => {
+      /* check whether Newsletter value is present
+       Then, Append data to the end of current url to use URL() for parsing */
+      news = chunk.toString();
+      if (news.search('Newsletter') === -1) {
+        new_url = original_url.href + `query?` + chunk + '&Newsletter=off';
+      } else {
+        new_url = original_url.href + `query?` + chunk;
+      }
+      let url = new URL(new_url, `http://${req.headers.host}`);
+      let params = url.searchParams;
+      let list = [];
+      console.log('data: ' + chunk);
+
+      // creates Row objects to store queries and values
+      function Row(query, value) {
+        this.query = query;
+        this.value = value;
+      }
+
+      // parse url to create a new Row object for each query-value pair and add to list array
+      params.forEach((second, first) => {
+        // checks whether checkbox is checked
+        if (first === 'Newsletter' && second === 'on') {
+          let add = new Row(first, 'Yes, sign me up for the newsletter.');
+          list.push(add);
+        }
+        // checks whether checkbox is unchecked
+        else if (first === 'Newsletter' && second === 'off') {
+          let add = new Row(first, 'No, thank you.');
+          list.push(add);
+        }
+        // checks whether there are inputs
+        else if (second !== '') {
+          let add = new Row(first, second);
+          list.push(add);
+        }
+        // an input left empty
+        else {
+          let add = new Row(first, 'n/a');
+          list.push(add);
+        }
+      });
+
+      // Create rows
+      list.forEach((row) => {
+        if (row.query !== 'submit') {
+          let col1 = row.query;
+          let col2 = row.value;
+          info = info + `${col1}: ${col2}<br>`;
+        }
+      });
+      info = info + `</p>`;
     });
-    res.write('You submitted the form');
-    res.end();
+
+    // Displays form info gathered
+    req.on('end', () => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      });
+      res.write(info);
+      res.end();
+    });
   }
 
-  // other routes --> invoke error page
+  // new route --> all other routes invoke 404
   else {
     res.writeHead(404, {
       'Content-Type': 'text/html',
@@ -54,8 +116,6 @@ const server = http.createServer((req, res) => {
     res.end();
   }
 });
-
-// http://localhost:5001/submit should return all the data the user entered
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
