1ac79c648b1536c00306cf66bfc18cba9fa01dd9
C3ra906 <95190216+C3ra906@users.noreply.github.com>
2022-11-07 01:16:34 -0800

    02 working great

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -3,13 +3,77 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 5001;
 
-// Add your code here
-
 // Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+    store: new session.MemoryStore(),
+    secret: 'my cat is super cute',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 86400000,
+    },
+  })
+);
 
+// main route
 app.get('/', (req, res) => {
-  // Add your code here
+  res.status(200);
+  res.type('txt');
+
+  // first domain visit
+  if (req.session.routes === undefined) {
+    req.session.routes = [];
+    res.write(`currently on route: ` + req.route.path + '\n' + '\n');
+    res.write(`Welcome to http://localhost:${port}`);
+  }
+
+  // non-first domain visit
+  else {
+    res.write(`currently on route: ` + req.route.path + '\n' + '\n');
+    res.write(`Previously visited: ` + '\n');
+    req.session.routes.forEach((route) => {
+      res.write(route + '\n');
+    });
+  }
+
+  // store route
+  req.session.routes.push(req.route.path);
+
+  res.end();
+});
+
+// all other routes
+app.get('/:parameter', (req, res) => {
+  res.status(200);
+  res.type('txt');
+
+  // first domain visit
+  if (req.session.routes === undefined) {
+    req.session.routes = [];
+    res.write(`currently on route: /` + req.params.parameter + '\n' + '\n');
+    res.write(`Welcome to http://localhost:${port}`);
+  }
+
+  // non-first domain visit
+  else {
+    res.write(`currently on route: /` + req.params.parameter + '\n' + '\n');
+    res.write(`Previously visited: ` + '\n');
+    req.session.routes.forEach((route) => {
+      if (route !== '/') {
+        res.write(`/` + route + '\n');
+      } else {
+        res.write(route + '\n');
+      }
+    });
+  }
+
+  // store route if it's not /favicon.ico route
+  if (req.params.parameter !== 'favicon.ico') {
+    req.session.routes.push(req.params.parameter);
+  }
+
+  res.end();
 });
 
 app.listen(port, () => {
